c560c5bd4d8531c5df3ab7828c8c979776fcb143
Vidimaster <vidimaster@yandex.com>
2024-06-19 15:21:22 +0700

    local storage js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         containerEL.insertAdjacentHTML("beforeend", `
             
                <div class="item_content">
-                <a class="item-img" href="single_page.html">
+                <a class="item-img">
 
                     <img class="item-img-pic" src="${element.img}" alt="">
                     <div class="txt-box">
@@ -27,18 +27,62 @@ document.addEventListener("DOMContentLoaded", async () => {
 
                 </a>
 
-                <div class="add-box">
-                    <a class="add"> <i class="fa fa-solid fa-cart-shopping" style="font-size: 14px;"><span
+                <div class="add-box" id="itm${element.id}">
+                    <a class="add"> <i class="fa fa-solid fa-cart-shopping" style="font-size: 14px;"><span 
                                 style="    font-family: Lato, sans-serif;"> Add to
                                 Cart</span></i> </a>
                 </div>
             </div>         
             
             `)
+    });
 
-        containerEL.addEventListener("click", el => {
-            console.log(`${element.title} added to cart`);
-        })
+    containerEL.addEventListener("click", function (e) {
+        if (e.target.closest(".add-box") != null) {
+            localStorage.setItem(e.target.closest(".add-box").id, JSON.stringify({ id: e.target.closest(".add-box").id }));
+            location.reload();
+        }
+    });
+
+    const CartItemEL = document.querySelector('.new_cart')
+
+    if (localStorage.length > 0) {
+        CartItemEL.insertAdjacentHTML("beforebegin", `<h2 class="cart_header">Cart Items</h2>`)
+        for (let i = 0; i < localStorage.length; i++) {
+            let str = JSON.parse(localStorage.getItem(localStorage.key(i)))
+
+            CartItemEL.insertAdjacentHTML("beforeend", `
+                
+                <div class="cart_container">
+                    <div class="container-left-cart item_in_cart"> <img src="${data[str.id.slice(-1)].img}" alt="" class="item-img">
+                    </div>
+                    <div class="container-right-cart item_in_cart">
+                        <p class="content-font-card-header">MANGO PEOPLE T-SHIRT</p>
+    
+                        <p class="content-font-card-price" style="color: #F16D7F">Price: ${data[str.id.slice(-1)].price}</p>
+                        <p class="content-font-card-price">Color: ${data[str.id.slice(-1)].color}</p>
+                        <p class="content-font-card-price">Size: ${data[str.id.slice(-1)].size}</p>
+                        <label for="name${data[str.id.slice(-1)].size}">Quantity: </label>
+                        <input placeholder="${data[str.id.slice(-1)].quantity}" type="text" name="name${data[str.id.slice(-1)].size}" required minlength="1" maxlength="1" size="2" />
+                    </div>
+                    <div class="container-right-cart"><button id="remove${str.id.slice(-1)}" class="btn-cart-remove"><span
+                                style="    font-family: Lato, sans-serif;">X</span></button></div>
+                </div>      
+         
+         `)
+        }
+    }
+
+
+
+    CartItemEL.addEventListener("click", function (e) {
+        if (e.target.closest(".btn-cart-remove") != null) {
+            localStorage.removeItem('itm' + e.target.closest(".btn-cart-remove").id.slice(-1));
+            location.reload();
+        }
     });
 
 });
+
+
+
